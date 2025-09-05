b9b32546098347e338493183e667dde20952b598
snufflespt <nunolavajo.pt@gmail.com>
2025-09-05 10:37:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,22 @@
 import streamlit as st
-from secoes import utentes, turmas, professores
+from secoes import utentes, turmas, professores, disciplinas
 
 st.set_page_config(page_title="Gestão IPSS", page_icon="🧭", layout="wide")
 
 st.sidebar.title("Menu")
 opcao = st.sidebar.radio(
     "Escolhe a secção:",
-    ["🏠 Início", "🧍 Utentes", "📚 Turmas", "👨‍🏫 Professores"]
+    ["🏠 Início", "📚 Disciplinas", "🧍 Utentes", "🏫 Turmas", "👨‍🏫 Professores"]
 )
 
 if opcao == "🏠 Início":
     st.title("Bem-vindo à Gestão IPSS")
     st.write("Usa o menu à esquerda para navegar entre as secções.")
+elif opcao == "📚 Disciplinas":
+    disciplinas.mostrar_pagina()
 elif opcao == "🧍 Utentes":
     utentes.mostrar_pagina()
-elif opcao == "📚 Turmas":
+elif opcao == "🏫 Turmas":
     turmas.mostrar_pagina()
 elif opcao == "👨‍🏫 Professores":
     professores.mostrar_pagina()
